216d46578c6cd00306609385748fe2fa4493e807
Stevie Holdway <stevie.holdway@gmail.com>
2024-07-24 13:40:37 -0400

    added edit flags func

--- lisp/config-management.el
+++ lisp/config-management.el
@@ -8,6 +8,10 @@
   (interactive)
   (load "~/.emacs.d/init.el"))
 
+(defun edit-flags ()
+  (interactive)
+  (find-file "~/.emacs.d/lisp/config-management.el"))
+
 ;; now set flags for packages
 (setq flag/editorconfig 't)
 (setq flag/user-font 't)
